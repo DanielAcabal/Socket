a22160436fd4bcbf99788ac52c991bb8cf87c513
DanielAcabal <danchiacabal@gmail.com>
2024-11-16 22:18:42 -0600

    Services organized

--- test/app.py
+++ test/app.py
@@ -1,6 +1,8 @@
 import websockets
 import json
 import os
+import asyncio
+import threading
 from src.connector import insertar_datos
 from dotenv import load_dotenv
 
@@ -8,19 +10,23 @@ load_dotenv()
 ip = os.getenv("IP")
 port = os.getenv("PORT")
 
-
-# create handler for each connection
 async def handler(websocket, path):
     data = await websocket.recv()
     content = json.loads(data)
     insertar_datos(content["mac"],content["temp"],content["humidity"],content["time"])
     print(data)
     reply = f"Data recieved!"+data
-    #print(ServerConnection.latency)
-    #await asyncio.sleep(2)
     await websocket.send(reply)
 
 def main():
-    #asyncio.get_event_loop().run_until_complete(start_server)
-    start_server = websockets.serve(handler, ip, port)
-    #asyncio.get_event_loop().run_forever()
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    ws_server = websockets.serve(handler, ip, port)
+
+    loop.run_until_complete(ws_server)
+    loop.run_forever() 
+    loop.close()
+
+if __name__ == "__main__":
+    _thread = threading.Thread(target=main)
+    _thread.start()
